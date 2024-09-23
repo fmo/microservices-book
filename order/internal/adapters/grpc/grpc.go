04c4130dcdae7f3151ef2215c10aeb214a6a3e9c
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-09-23 21:41:43 +0200

    validations

--- order/internal/adapters/grpc/grpc.go
+++ order/internal/adapters/grpc/grpc.go
@@ -5,10 +5,28 @@ import (
 	"github.com/fmo/microservices-book/order/internal/application/core/domain"
 	"github.com/fmo/microservices-proto/golang/order"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/status"
 )
 
 func (a Adapter) Create(ctx context.Context, request *order.CreateOrderRequest) (*order.CreateOrderResponse, error) {
 	log.WithContext(ctx).Info("Creating order...")
+
+	var validationErrors []*errdetails.BadRequest_FieldViolation
+	if request.UserId < 1 {
+		validationErrors = append(validationErrors, &errdetails.BadRequest_FieldViolation{
+			Field:       "user_id",
+			Description: "user id cannot be less than 1",
+		})
+	}
+	if len(validationErrors) > 0 {
+		stat := status.New(400, "invalid order request")
+		badRequest := &errdetails.BadRequest{}
+		badRequest.FieldViolations = validationErrors
+		s, _ := stat.WithDetails(badRequest)
+		return nil, s.Err()
+	}
+
 	var orderItems []domain.OrderItem
 	for _, orderItem := range request.OrderItems {
 		orderItems = append(orderItems, domain.OrderItem{
