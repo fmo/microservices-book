ed6c742f26bbdcca0e287a5bfad9b0fb58a50714
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-11-05 21:28:44 +0100

    tests

--- order/internal/application/core/api/api_test.go
+++ order/internal/application/core/api/api_test.go
@@ -3,7 +3,9 @@ package api
 import (
 	"context"
 	"github.com/fmo/microservices-book/order/internal/application/core/domain"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"testing"
 )
 
 type mockedPayment struct {
@@ -27,4 +29,25 @@ func (d *mockedDb) Save(ctx context.Context, order *domain.Order) error {
 func (d *mockedDb) Get(ctx context.Context, id int64) (domain.Order, error) {
 	args := d.Called(ctx, id)
 	return args.Get(0).(domain.Order), args.Error(1)
+}
+
+func Test_Should_Place_Order(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+	payment.On("Charge", mock.Anything, mock.Anything).Return(nil)
+	db.On("Save", mock.Anything, mock.Anything).Return(nil)
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(context.Background(), domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "camera",
+				UnitPrice:   12.3,
+				Quantity:    3,
+			},
+		},
+		CreatedAt: 0,
+	})
+	assert.Nil(t, err)
 }
