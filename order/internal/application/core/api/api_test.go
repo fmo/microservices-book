ca07f1a68ddee8149f93f7e2d5a9f21d49a2d566
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-11-09 20:20:26 +0100

    mocks tests

--- order/internal/application/core/api/api_test.go
+++ order/internal/application/core/api/api_test.go
@@ -2,9 +2,13 @@ package api
 
 import (
 	"context"
+	"errors"
 	"github.com/fmo/microservices-book/order/internal/application/core/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"testing"
 )
 
@@ -50,4 +54,49 @@ func Test_Should_Place_Order(t *testing.T) {
 		CreatedAt: 0,
 	})
 	assert.Nil(t, err)
+}
+
+func Test_Should_Return_Error_When_Db_Persistence_Fail(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+	payment.On("Charge", mock.Anything, mock.Anything).Return(nil)
+	db.On("Save", mock.Anything, mock.Anything).Return(errors.New("connection error"))
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(context.Background(), domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "phone",
+				UnitPrice:   14.7,
+				Quantity:    1,
+			},
+		},
+		CreatedAt: 0,
+	})
+	assert.EqualError(t, err, "connection error")
+}
+
+func Test_Should_Return_Error_When_Payment_Fail(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+	payment.On("Charge", mock.Anything, mock.Anything).Return(errors.New("insufficient balance"))
+	db.On("Save", mock.Anything, mock.Anything).Return(nil)
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(context.Background(), domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "bag",
+				UnitPrice:   2.5,
+				Quantity:    6,
+			},
+		},
+		CreatedAt: 0,
+	})
+	st, _ := status.FromError(err)
+	assert.Equal(t, st.Message(), "order creation failed")
+	assert.Equal(t, st.Details()[0].(*errdetails.BadRequest).FieldViolations[0].Description, "insufficient balance")
+	assert.Equal(t, st.Code(), codes.InvalidArgument)
 }
